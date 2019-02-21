faf0fa5f82aeb7eb6ea549b0dfa11986a8d128e4
gauravkerkar <gauravkerkar1999@gmail.com>
2019-02-21 11:45:15 +0530

    added sidebar icons

--- app/src/main/java/com/example/smartshare/URLs.java
+++
@@ -1,8 +0,0 @@
-package com.example.smartshare;
-
-public class URLs {
-    private static final String ROOT_URL = "http://192.168.43.107/Android/Api.php?apicall=";
-
-    public static final String URL_REGISTER = ROOT_URL + "signup";
-    public static final String URL_LOGIN= ROOT_URL + "login";
-}
